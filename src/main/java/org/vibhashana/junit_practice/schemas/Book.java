ecf9676ac9565d98720786c8edfc9b132449ed1a
vibhashan <ranhindavibhashana@gmail.com>
2025-03-31 16:49:54 +0530

    feat: Book schema implemented

---
+++ src/main/java/org/vibhashana/junit_practice/schemas/Book.java
@@ -0,0 +1,16 @@
+package org.vibhashana.junit_practice.schemas;
+
+import lombok.Data;
+
+@Data
+public class Book {
+	private int id;
+	private String name;
+	private double price;
+	private String author;
+
+	@Override
+	public String toString() {
+		return String.format("ID: %d, Name: %s, Price: %d, Author: %s", id, name, price, author);
+	}
+}
